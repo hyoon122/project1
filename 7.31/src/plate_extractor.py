119a6880dba86f0c1669cec010684dc692094271
hyoon122 <hoyoonjo6@gmail.com>
2025-07-31 12:05:07 +0900

    마우스 클릭 함수 지정
    번호판 영역 기준 좌상단-우상단-우하단-좌하단 순으로 클릭하여 영역을 지정하는 마우스 클릭 함수 제작

--- 7.31/src/plate_extractor.py
+++ 7.31/src/plate_extractor.py
@@ -21,19 +21,37 @@ if img is None:
     print(f"이미지를 읽을 수 없습니다: {img_path}")
     exit()
 
+draw = img.copy()
+pts = np.zeros((4, 2), dtype=np.float32)
+pts_cnt = 0
+
 # 번호판 영역 기준, 좌상단-> 우상단-> 우하단-> 좌하단 모서리 클릭
-sm = pts.sum(axis=1)           # x + y 값 계산
-diff = np.diff(pts, axis=1)    # x - y 값 계산
-topLeft = pts[np.argmin(sm)]        # x+y가 최소 → 좌상단
-bottomRight = pts[np.argmax(sm)]    # x+y가 최대 → 우하단
-topRight = pts[np.argmin(diff)]     # x-y가 최소 → 우상단
-bottomLeft = pts[np.argmax(diff)]   # x-y가 최대 → 좌하단
 
-# 표준 번호판 크기로 고정
+def onMouse(event, x, y, flags, param):
+    global pts_cnt, pts
 
-width = 300
-height = 150
-pts2 = np.float32([[0,0], [width-1,0], [width-1,height-1], [0,height-1]])
+    if event == cv2.EVENT_LBUTTONDOWN and pts_cnt < 4:
+        cv2.circle(draw, (x, y), 5, (0, 255, 0), -1)
+        cv2.imshow(win_name, draw)
+
+        pts[pts_cnt] = [x, y]
+        pts_cnt += 1
+
+        if pts_cnt == 4:
+            # 1. 좌표 정렬
+            sm = pts.sum(axis=1)                # x + y 값 계산
+            diff = np.diff(pts, axis=1)         # x - y 값 계산
+            topLeft = pts[np.argmin(sm)]        # x+y가 최소 → 좌상단
+            bottomRight = pts[np.argmax(sm)]    # x+y가 최대 → 우하단
+            topRight = pts[np.argmin(diff)]     # x-y가 최소 → 우상단
+            bottomLeft = pts[np.argmax(diff)]   # x-y가 최대 → 좌하단
+
+            # 좌상단 -> 우상단 -> 우하단 -> 좌하단
+            pts1 = np.float32([topLeft, topRight, bottomRight, bottomLeft])
+            
+            # 2. 표준 번호판 크기 지정
+            width, height = 300, 150
+            pts2 = np.float32([[0,0], [width-1,0], [width-1,height-1], [0,height-1]])
 
 
 # # 방식 1: 타임스탬프 기반
