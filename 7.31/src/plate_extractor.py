78206d92891baf67d25351e6118e8fdd19e84f1b
hyoon122 <hoyoonjo6@gmail.com>
2025-07-31 11:49:05 +0900

    개요 작성
    프로젝트의 방향성을 대략적으로 구상하는 과정.

--- 7.31/src/plate_extractor.py
+++ 7.31/src/plate_extractor.py
@@ -1,4 +1,7 @@
 # 자동차 번호판 추출 실습 가이드
+import os
+import cv2
+import numpy as np
 
 
 # 번호판 영역 기준, 좌상단-> 우상단-> 우하단-> 좌하단 모서리 클릭
@@ -14,3 +17,29 @@ bottomLeft = pts[np.argmax(diff)]   # x-y가 최대 → 좌하단
 width = 300
 height = 150
 pts2 = np.float32([[0,0], [width-1,0], [width-1,height-1], [0,height-1]])
+
+
+# # 방식 1: 타임스탬프 기반
+# import datetime
+# timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+# filename = f"extracted_plates/plate_{timestamp}.jpg"
+
+# 방식 2: 순번 기반 (해당 방식을 채택, import os 필요)
+
+existing_files = len(os.listdir("extracted_plates"))
+filename = f"extracted_plates/plate_{existing_files+1:03d}.jpg"
+
+# < 저장 코드 구현 단계 >
+# 원근변환 완료 후 자동 저장
+
+# float형으로 입력되어 있을 경우, int형으로 변환해서 출력
+result = cv2.warpPerspective(img, mtrx, (int(width), int(height)))
+
+# 파일 저장
+success = cv2.imwrite(filename, result)
+
+if success:
+    print(f"번호판 저장 완료: {filename}")
+    cv2.imshow('Extracted Plate', result)
+else:
+    print("저장 실패!")
